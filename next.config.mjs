79951e96dcb29fde81ebbec3c1967d08d424a68a
JARV005 <johanalexanderriveravasquez@gmail.com>
2025-12-15 21:17:12 -0500

    fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  basePath: '/Portafolios_Coders',
+  assetPrefix: '/Portafolios_Coders/',
   images: {
-    unoptimized: true,
-  },
+    unoptimized: true
+  }
 }
 
-export default nextConfig
+module.exports = nextConfig
